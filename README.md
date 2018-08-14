90729c692a2e30b678f71bdc7fc44dffd56dd6ec
elishakay <kramer1346@gmail.com>
2018-08-14 12:43:07 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Inventory-Dashboard
+
+<img src="dashboard.png">
+
+Features:
+
+- Add Product
+- Delete Product
+- Update Product
+- Save Updated Products List in LocalStorage
--- js/app.js
+++ js/app.js
@@ -3,27 +3,19 @@ angular.module('myApp', ["ngStorage"]);
 angular.module('myApp').controller('main', function ($scope,  $localStorage) {
 
     if(typeof $localStorage.productList != 'undefined'){
-        console.log('found productList in localStorage');
         $scope.productList = $localStorage.productList;
-        console.log('here is the productList',$scope.productList);
-
     } else {
-        
-        
-        console.log('couldnt find productList in localStorage');
         $scope.productList = [{id: 1, name: "coffee", description: " tasty coffee", price: 6, image_src: "img/coffee.jpg"},
                             {id: 2, name: "chocolate", description: " tasty chocolate", price: 12, image_src: "img/chocolate.jpeg"},
                             {id: 3, name: "sugar", description: "tasty sugar", price: 18, image_src: "img/sugar.jpg"}]
-        console.log('here is the productList',$scope.productList);
     }
-
-
 
     $scope.updateProductMode = false;
     $scope.addProductMode = false;
 
     //Mark selected div
     $scope.idSelectedProduct = null;
+
     $scope.setSelected = function (idSelectedProduct) {
         $scope.updateProductMode = true;
         $scope.idSelectedProduct = idSelectedProduct;
@@ -49,13 +41,12 @@ angular.module('myApp').controller('main', function ($scope,  $localStorage) {
 
     $scope.removeProduct = function(productId){
         var removeIndex = $scope.productList.map(function(item) { return item.id; }).indexOf(productId);
-        // remove object
         $scope.productList.splice(removeIndex, 1);
-        console.log(productId);
         //update ids of objects in array - for maintaining selected product feature
         for (i = 0; i < $scope.productList.length; i++) { 
             $scope.productList[i].id = i+1;
         }
+        $scope.idSelectedProduct = null;
     }
     
     $scope.saveProductChanges = function(editedProduct){
@@ -67,5 +58,4 @@ angular.module('myApp').controller('main', function ($scope,  $localStorage) {
         }
     }
 
-       console.log('log this');
 });
